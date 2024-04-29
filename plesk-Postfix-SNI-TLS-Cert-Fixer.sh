6c82a7b6b32e2729c62209c67417d8d5092c8264
Andreas Schnederle-Wagner <schnederle@futureweb.at>
2024-04-29 15:47:30 +0200

    Update plesk-Postfix-SNI-TLS-Cert-Fixer.sh

--- plesk-Postfix-SNI-TLS-Cert-Fixer.sh
+++ plesk-Postfix-SNI-TLS-Cert-Fixer.sh
@@ -21,22 +21,59 @@
 
 PLESK_BIN="/usr/sbin/plesk"
 
+# Initialize counters for statistics
+total_domains=0
+renewed_domains=0
+errors=0
+
+# Function to print error message and increment error count
+print_error() {
+    echo "Error: $1"
+    ((errors++))
+}
+
 # Loop through mail domains
-for domain in $($PLESK_BIN bin domain --list | grep '^mail\.\|^[^.]*$'); do
+while IFS= read -r domain; do
+    ((total_domains++))
+
     # Trim "mail." prefix if it exists
     domain_without_prefix="${domain#mail.}"
 
     # Get the SSL/TLS certificate for mail for the current domain
-    mail_certificate=$($PLESK_BIN bin subscription_settings --info $domain_without_prefix | grep -oP 'SSL/TLS certificate for mail:\s*\K.*')
+    mail_certificate=$("$PLESK_BIN" bin subscription_settings --info "$domain_without_prefix" | grep -oP 'SSL/TLS certificate for mail:\s*\K.*')
 
-    # Print the mail certificate
-    #echo "Mail certificate: $mail_certificate"
+    if [[ -z "$mail_certificate" ]]; then
+        print_error "Failed to retrieve SSL/TLS certificate for $domain_without_prefix"
+        continue
+    fi
 
     if [[ "$mail_certificate" == "Lets Encrypt $domain" ]]; then
         echo "Renew & Re-Assign Cert for $domain_without_prefix Mail Services with Cert: $mail_certificate"
-        $PLESK_BIN bin certificate --update "$mail_certificate" -domain $domain
-        $PLESK_BIN bin subscription_settings --update $domain_without_prefix -mail_certificate "mail_certificate"
+        if ! "$PLESK_BIN" bin certificate --update "$mail_certificate" -domain "$domain"; then
+            print_error "Failed to renew certificate for $domain_without_prefix"
+            continue
+        fi
+
+        if ! "$PLESK_BIN" bin subscription_settings --update "$domain_without_prefix" -mail_certificate "$mail_certificate"; then
+            print_error "Failed to update mail certificate for $domain_without_prefix"
+            continue
+        fi
+
+        ((renewed_domains++))
     else
         echo "Mail certificate is not 'Lets Encrypt for $domain_without_prefix'"
     fi
-done
+done < <("$PLESK_BIN" bin domain --list | grep '^mail\.\|^[^.]*$')
+
+# Print run statistics
+echo "Script finished processing $total_domains domains."
+echo "$renewed_domains domains had their certificates renewed."
+echo "$errors errors occurred during script execution."
+
+# Restart Postfix
+echo "Restarting Postfix..."
+if /usr/sbin/service postfix restart; then
+    echo "Postfix restarted successfully."
+else
+    print_error "Failed to restart Postfix."
+fi
